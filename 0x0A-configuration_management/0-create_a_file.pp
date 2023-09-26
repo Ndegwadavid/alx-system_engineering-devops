173f21ee2b85baa969eecee307ed7571e806e814
david Njoroge <davidndegwa6869@gmail.com>
2023-09-26 10:07:21 +0300

    all done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# Creates a file
 file { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
-  content => "I love Puppet\n",
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
